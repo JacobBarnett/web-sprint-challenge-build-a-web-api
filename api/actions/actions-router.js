71c5c332f33b0cb5b78b7713681fb6c36be5174b
JacobBarnett <93158517+JacobBarnett@users.noreply.github.com>
2022-06-01 14:30:46 -0700

    test

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,11 @@
 // Write your "actions" router here!
+const { Router } = require("express");
+const db = require("../../data/dbConfig.js");
+const model = require("./projects-model");
+
+const router = new Router();
+
+router.get("/", async (req, res) => {
+  const actions = await db("actions");
+  res.send(actions);
+});
